6e53591d20b1225eb9e2660409110a10c1c8955a
Titouan Mendiharat <titouam@gmail.com>
2025-08-13 15:13:44 -0700

    Increased cooldown for calibration

--- utils/constants.py
+++ utils/constants.py
@@ -11,7 +11,7 @@ NOD_COOLDOWN = pd.Timedelta(seconds=2.0)
 
 CALIBRATION_WAIT_S = 10.0       # Time window to wait for the gesture per IP
 CALIBRATION_WAIT_TRIGGER = 3.0  # Time window for double-trigger
-CALIBRATION_COOLDOWN = pd.Timedelta(seconds=2.0) # Cooldown between gestures in calibration mode
+CALIBRATION_COOLDOWN = pd.Timedelta(seconds=5.0) # Cooldown between gestures in calibration mode
 
 SERVICE_UUID = "00000000-0004-11e1-9ab4-0002a5d5c51b"
 CHARACTERISTIC_01 = "00000001-0004-11e1-ac36-0002a5d5c51b"  # Notify
